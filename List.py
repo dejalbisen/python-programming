743af185bda1ec0fb64b48347415719a63d2bec7
dejalbisen <57594197+dejalbisen@users.noreply.github.com>
2021-05-24 16:44:42 +0530

    Add files via upload

---
+++ List.py
@@ -0,0 +1,21 @@
+#create a list using []
+a=[1,2,4,56,6]
+
+# print the list using print() Functions
+print(a)
+
+# Access using index using a[0],a[1],a[2]
+print(a[2])
+
+# change the value of list using
+a[0]=98
+print(a)
+
+# We can create a list with items of different types
+#  c = [45,"Dejal", false, 6.8]
+#  print(c)
+
+#  list slicining
+friends = ["Harry", "Tom", "Sam","Divya",45]
+print(friends[0:4])
+print(friends[-4:])
---
+++ Variable.py
@@ -0,0 +1,13 @@
+a ="Dejal"
+b =333
+c =45.11 
+
+# ptinting the variabel
+print(a)
+print(b)
+print(c)
+
+
+# printing the type of variables
+print(type(a))
+print(type(b))
